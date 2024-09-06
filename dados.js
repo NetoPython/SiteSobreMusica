1bcce81731b985e112e076f32544723d5ba01a92
NetoPython <netoprogramador1999@gmail.com>
2024-09-06 14:57:39 -0300

    adicionei mais uma musica

--- dados.js
+++ dados.js
@@ -1,31 +1,67 @@
 let dados = [
-     {
-        titulo : "Hotel California",
-        descrição : "A canção Hotel California dos Eagles é, sem dúvida, um dos maiores clássicos do rock, mas também um dos mais enigmáticos. Suas letras, cheias de simbolismo e metáforas, deram origem a inúmeras teorias e interpretações ao longo dos anos.",
-        link : "https://youtu.be/09839DpTctU?si=2Y6PbPINYDdDjpjc"
-                 
-            
-    },
-        {
-           titulo : "Dumingaz - Jorge Ben Jor",
-           descrição : "A música 'Dumingaz' de Jorge Ben Jor é uma celebração da simplicidade e da beleza dos momentos compartilhados com alguém especial. A repetição do nome 'Dumingaz' e o uso de palavras como 'maravilha' e 'sensual' criam uma atmosfera de alegria e desejo. A letra expressa um anseio profundo por um domingo perfeito, sem chuva, onde o eu lírico possa passear no parque de mãos dadas com a pessoa amada. Esse desejo é intensificado pela saudade, evidenciada na repetição de 'mais um domingo sem você, já não aguento mais'.",
-           link : "https://youtu.be/5amJ03syFFs?si=74kCH3X4FCc1m008",
-                    
-               
-       },
-
-       {
-        titulo : "Something About You - Eyedress",
-        descrição : "A música 'Something About You', uma colaboração entre Eyedress e Dent May, é uma ode ao romance e à conexão íntima que se desenvolve entre duas pessoas. A letra descreve uma série de momentos compartilhados entre o narrador e sua amada, enfatizando a sensação de contentamento e atração que ele sente por ela. A repetição do verso 'In the car, cruising around with you' sugere uma sensação de liberdade e prazer nas pequenas aventuras cotidianas ao lado da pessoa amada.",
-        link : "https://youtu.be/0VAAS9xnS5U?si=NlCW0uFVMOC2RZNw",
-                 
-            
-    },
-        {
-          titulo : "Gangsta's Paradise - Coolio",
-          descrição : "A música 'Gangsta's Paradise', do rapper Coolio, é um retrato sombrio e introspectivo da vida nas ruas e do envolvimento com gangues. Lançada em 1995, a canção se tornou um marco do hip-hop dos anos 90 e é reconhecida por sua abordagem crítica e reflexiva sobre as consequências da violência e da criminalidade. A letra começa com uma referência bíblica ao 'vale da sombra da morte', sugerindo que o narrador está caminhando por um lugar perigoso e potencialmente mortal, que é a realidade da vida nas gangues.",
-          link : "https://youtu.be/fPO76Jlnz6c?si=iafvEsGOhTnywvIy"
-        }
     
-];
+    
+    {
+        titulo: "Hotel California - Eagles",
+        descrição: "A canção Hotel California dos Eagles é, sem dúvida, um dos maiores clássicos do rock, mas também um dos mais enigmáticos. Suas letras, cheias de simbolismo e metáforas, deram origem a inúmeras teorias e interpretações ao longo dos anos.",
+        link: "https://youtu.be/09839DpTctU?si=2Y6PbPINYDdDjpjc",
+        tags : "Eagles estrada hospedar"
+
+
+    },
+
+    {
+        titulo: "Dumingaz - Jorge Ben Jor",
+        descrição: "A música 'Dumingaz' de Jorge Ben Jor é uma celebração da simplicidade e da beleza dos momentos compartilhados com alguém especial. A repetição do nome 'Dumingaz' e o uso de palavras como 'maravilha' e 'sensual' criam uma atmosfera de alegria e desejo. A letra expressa um anseio profundo por um domingo perfeito, sem chuva, onde o eu lírico possa passear no parque de mãos dadas com a pessoa amada. Esse desejo é intensificado pela saudade, evidenciada na repetição de 'mais um domingo sem você, já não aguento mais'.",
+        link: "https://youtu.be/5amJ03syFFs?si=74kCH3X4FCc1m008",
+        tags : "samba domingo chover "
+
+
+    },
+
+    {
+        titulo: "Something About You - Eyedress",
+        descrição: "A música 'Something About You', uma colaboração entre Eyedress e Dent May, é uma ode ao romance e à conexão íntima que se desenvolve entre duas pessoas. A letra descreve uma série de momentos compartilhados entre o narrador e sua amada, enfatizando a sensação de contentamento e atração que ele sente por ela. A repetição do verso 'In the car, cruising around with you' sugere uma sensação de liberdade e prazer nas pequenas aventuras cotidianas ao lado da pessoa amada.",
+        link: "https://youtu.be/0VAAS9xnS5U?si=NlCW0uFVMOC2RZNw",
+        tags : "praia amor love song"
+
+
+    },
+
+    {
+        titulo: "Gangsta's Paradise - Coolio",
+        descrição: "A música 'Gangsta's Paradise', do rapper Coolio, é um retrato sombrio e introspectivo da vida nas ruas e do envolvimento com gangues. Lançada em 1995, a canção se tornou um marco do hip-hop dos anos 90 e é reconhecida por sua abordagem crítica e reflexiva sobre as consequências da violência e da criminalidade. A letra começa com uma referência bíblica ao 'vale da sombra da morte', sugerindo que o narrador está caminhando por um lugar perigoso e potencialmente mortal, que é a realidade da vida nas gangues.",
+        link: "https://youtu.be/fPO76Jlnz6c?si=iafvEsGOhTnywvIy",
+        tags : "rap lenda geração"
+    },
+
+    {
+        titulo: "Another Brick in the Wall (Pt. 2) - Pink Floyd",
+
+        descrição: "A canção 'Another Brick In The Wall (Pt. 2)', da banda britânica Pink Floyd, é um dos maiores clássicos do rock e uma forte crítica ao sistema educacional rígido e opressor. Lançada em 1979, no álbum 'The Wall', a música se tornou um hino de protesto contra a conformidade e a perda da individualidade, temas recorrentes na obra da banda. A letra, escrita pelo baixista Roger Waters, reflete a insatisfação com uma educação que é percebida como uma tentativa de controle do pensamento e uma supressão da criatividade e da expressão individual.",
+        link: "https://open.spotify.com/intl-pt/track/4gMgiXfqyzZLMhsksGmbQV?si=8c54ccef0b7c4188",
+        tags : "education hey chichers"
+
+    },
+
+    {
+        titulo: "UP - Forrest Frank",
+        descrição: "A música 'UP!' de Forrest Frank, com participação de Connor Price, é uma expressão vibrante de superação e fé. A letra descreve uma transformação pessoal, onde o eu lírico passa de um estado de desânimo e dificuldade ('down') para um de elevação e sucesso ('up'). A repetição do contraste entre estar 'down' e agora estar 'up' enfatiza a mudança significativa na vida do narrador, atribuindo essa mudança à intervenção divina ('This all God this ain't no luck'), rejeitando a ideia de que a sorte seja responsável por essa reviravolta.",
+        link: "https://youtu.be/wCw-W-2Rs0k?si=CrI1YbqqvyqBcdEX",
+        tags : "cristo jesus cristã"
+
+    },
+    
+
+
+    {
+        titulo: "The Dark Side of the Moon",
+        descrição: "The Dark Side of the Moon é o oitavo álbum de estúdio da banda britânica de rock progressivo Pink Floyd, lançado em 1 de março de 1973 através da Harvest Records no Reino Unido e Capitol Records nos Estados Unidos. O disco marca uma nova fase no som da banda, com letras mais pessoais e instrumentais menores, contendo alguns dos mais complicados usos dos instrumentos e efeitos sonoros existentes na época, incluindo o som de alguém correndo à volta de um microfone e a gravação de múltiplos relógios a tocar ao mesmo tempo.",
+        link: "https://open.spotify.com/intl-pt/album/4LH4d3cOWNNsVw41Gqt2kv?si=KOCTzUavR7m7Mh_6sz38UQ",
+        tags : "album bom"
+
+    }
+
+]
+
 
